2618cf2b8bac2df5478d3246acc2558a11250b9c
Jake <54716860+JakevdKolk@users.noreply.github.com>
2025-08-24 17:39:03 +0200

    added config files for vue

---
+++ dnd_initiative_frontend/src/components/Users.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref, onMounted } from "vue";
+import type { AxiosError } from "axios";
+import type { User } from "@/types/user";
+import { getUsers } from "../lib/api/user.ts"
+
+const users = ref<User[] | null>(null);
+const loading = ref(true);
+const error = ref<string | null>(null);
+
+onMounted(async () => {
+  try {
+    users.value = await getUsers();
+  } catch (err) {
+    const e = err as AxiosError<any>;
+    error.value = (e.response?.data as any)?.message ?? e.message;
+  } finally {
+    loading.value = false;
+  }
+});
+</script>
+
+<template>
+  <div class="p-4">
+    <p v-if="loading">Loading users…</p>
+    <p v-else-if="error" class="text-red-600">Error: {{ error }}</p>
+
+    <ul v-else>
+      <li v-for="u in users" :key="u.id">
+        {{ u.name  }}
+      </li>
+    </ul>
+  </div>
+</template>
---
+++ dnd_initiative_frontend/src/lib/api/user.ts
@@ -0,0 +1,11 @@
+import http from "@/lib/http";
+import type { User } from "@/types/user";
+
+// optionally set a baseURL somewhere central (vite proxy or full URL)
+// axios.defaults.baseURL = "/";
+
+export async function getUsers(): Promise<User[]> {
+  const { data } = await http.get<User[]>("/api/users");
+  console.log( 'data' , data);
+  return data;
+}
