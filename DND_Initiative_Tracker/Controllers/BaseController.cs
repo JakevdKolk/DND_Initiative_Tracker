f9c7873c00ebfd49ac0cc7f734b5a9e169fc1afa
Jake <54716860+JakevdKolk@users.noreply.github.com>
2025-09-12 10:45:00 +0200

    abstrtacted api into a new base controller

---
+++ DND_Initiative_Tracker/Controllers/BaseController.cs
@@ -0,0 +1,38 @@
+﻿using DND_Initiative_Tracker.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace DND_Initiative_Tracker.Controllers
+{
+    public abstract class BaseController<TEntity, TDto, TCreateDto, TKey> : Controller
+        where TEntity : class
+    {
+
+        protected readonly DbContext _dbContext;
+        protected readonly DbSet<TEntity> _dbSet;
+
+        public BaseController(DnDDbContext dbcontext)
+        {
+            this._dbContext = dbcontext;
+            this._dbSet = _dbContext.Set<TEntity>();
+        }
+
+        protected abstract Expression<Func<TEntity, TDto>> MapToDto();
+        protected abstract TEntity MapToEntity(TCreateDto dto);
+        protected abstract Expression<Func<TEntity, bool>> ById(TKey id);
+
+        [HttpGet]
+        public async Task<ActionResult<List<TDto>>> GetAll(CancellationToken ct) =>
+            Ok(await _dbSet.AsNoTracking().Select(MapToDto()).ToListAsync());
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TDto>> GetById([FromRoute] TKey id, CancellationToken ct)
+        {
+            var item = await _dbSet.AsNoTracking().Where(ById(id)).Select(MapToDto()).SingleOrDefaultAsync();
+            return item is null ? NotFound() : Ok(item);
+
+
+        }
+    }
+}
---
+++ DND_Initiative_Tracker/Controllers/CreatureController.cs
@@ -0,0 +1,44 @@
+﻿using DND_Initiative_Tracker.Contracts;
+using DND_Initiative_Tracker.Data;
+using DND_Initiative_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DND_Initiative_Tracker.Controllers
+{
+
+    [ApiController]
+    [Route("api/creatures")]
+    public class CreatureController : BaseController<Creature, CreatureDto, CreateCreatureDto, int>
+    {
+        public CreatureController(DnDDbContext context) : base(context) { }
+
+        protected override Expression<Func<Creature, CreatureDto>> MapToDto() => u => new(
+              u.Id, u.Name, u.Size, u.TypeId, u.Ac, u.Hp, u.HpDiceCount, u.HpDiceSize, u.HpDiceBonus,
+              u.LegendaryActionCharges, u.ProficiencyBonus, u.Notes, u.IsNpc, u.LairInitiative, u.InitiativeBonus, u.OwnerUserId
+        );
+
+        protected override Creature MapToEntity(CreateCreatureDto dto) => new()
+        {
+            Name = dto.Name,
+            Size = dto.Size,
+            TypeId = dto.TypeId,
+            Ac = dto.Ac,
+            Hp = dto.Hp,
+            HpDiceCount = dto.HpDiceCount,
+            HpDiceSize = dto.HpDiceSize,
+            HpDiceBonus = dto.HpDiceBonus,
+            LegendaryActionCharges = dto.LegendaryActionsCharges,
+            ProficiencyBonus = dto.ProficienyBonus,
+            Notes = dto.Notes,
+            IsNpc = dto.IsNpc,
+            LairInitiative = dto.LairInitiative,
+            InitiativeBonus = dto.InitiativeBonus,
+            OwnerUserId = dto.OwnerUserId
+        };
+
+        protected override Expression<Func<Creature, bool>> ById(int id) => u => u.Id == id;
+    }
+}
