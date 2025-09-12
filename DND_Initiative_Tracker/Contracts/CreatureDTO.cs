266b7f913fde44db0ffad5bfff10fbdfa1695ef5
JakeKolk <54716860+JakevdKolk@users.noreply.github.com>
2025-09-12 15:58:29 +0200

    Development (#18)
    * added indexes
    
    * Create README.md
    
    * fixed workflow files
    
    * optimized workflow files
    
    * added more migrations (#12)
    
    * added more migrations
    
    drop database by hand otherwise you get errors for some reason
    
    * added models and made migrations from models
    
    * fixed backend code quality
    
    * removed logger variables
    
    * created user and role routes (#13)
    
    * made base controller and added creature controller (#17)
    
    * Development (#14)
    
    * added indexes
    
    * Create README.md
    
    * fixed workflow files
    
    * optimized workflow files
    
    * added more migrations (#12)
    
    * added more migrations
    
    drop database by hand otherwise you get errors for some reason
    
    * added models and made migrations from models
    
    * fixed backend code quality
    
    * removed logger variables
    
    * created user and role routes (#13)
    
    * abstrtacted api into a new base controller
    
    * geworked role controller
    
    * reworked user controller
    
    * fixed double dtos
    
    * added update and delete to base controller
    
    * fixed warning and removed some commented code

---
+++ DND_Initiative_Tracker/Contracts/CreatureDTO.cs
@@ -0,0 +1,61 @@
+﻿
+
+using DND_Initiative_Tracker.Models.Enums;
+
+namespace DND_Initiative_Tracker.Contracts
+{
+    public record CreatureDto(
+        int Id,
+        string Name,
+        Size? Size,
+        int? TypeId,
+        int? Ac,
+        int? Hp,
+        int? HpDiceCount,
+        int? HpDiceSize,
+        int? HpDiceBonus,
+        int? LegendaryActionsCharges,
+        int? ProficienyBonus,
+        string? Notes,
+        bool? IsNpc,
+        int? LairInitiative,
+        int? InitiativeBonus,
+        int? OwnerUserId
+    );
+
+    public record CreateCreatureDto(
+        string Name,
+        Size? Size,
+        int? TypeId,
+        int? Ac,
+        int? Hp,
+        int? HpDiceCount,
+        int? HpDiceSize,
+        int? HpDiceBonus,
+        int? LegendaryActionsCharges,
+        int? ProficienyBonus,
+        string? Notes,
+        bool? IsNpc,
+        int? LairInitiative,
+        int? InitiativeBonus,
+        int? OwnerUserId
+    );
+
+    public record UpdateCreatureDto(
+        string Name,
+        Size? Size,
+        int? TypeId,
+        int? Ac,
+        int? Hp,
+        int? HpDiceCount,
+        int? HpDiceSize,
+        int? HpDiceBonus,
+        int? LegendaryActionsCharges,
+        int? ProficienyBonus,
+        string? Notes,
+        bool? IsNpc,
+        int? LairInitiative,
+        int? InitiativeBonus,
+        int? OwnerUserId
+     );
+}
---
+++ DND_Initiative_Tracker/Controllers/CreatureController.cs
@@ -0,0 +1,65 @@
+﻿using DND_Initiative_Tracker.Contracts;
+using DND_Initiative_Tracker.Data;
+using DND_Initiative_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DND_Initiative_Tracker.Controllers
+{
+
+    [ApiController]
+    [Route("api/creatures")]
+    public class CreatureController : BaseController<Creature, CreatureDto, CreateCreatureDto, UpdateCreatureDto, int>
+    {
+        public CreatureController(DnDDbContext context) : base(context) { }
+
+        protected override Expression<Func<Creature, CreatureDto>> MapToDto() => u => new CreatureDto(
+              u.Id, u.Name, u.Size, u.TypeId, u.Ac, u.Hp, u.HpDiceCount, u.HpDiceSize, u.HpDiceBonus,
+              u.LegendaryActionCharges, u.ProficiencyBonus, u.Notes, u.IsNpc, u.LairInitiative, u.InitiativeBonus, u.OwnerUserId
+        );
+
+        protected override void ApplyUpdate(Creature e, UpdateCreatureDto dto)
+        {
+            e.Name = dto.Name;
+            e.Size = dto.Size;
+            e.TypeId = dto.TypeId;
+            e.Ac = dto.Ac;
+            e.Hp = dto.Hp;
+            e.HpDiceCount = dto.HpDiceCount;
+            e.HpDiceSize = dto.HpDiceSize;
+            e.HpDiceBonus = dto.HpDiceBonus;
+            e.LegendaryActionCharges = dto.LegendaryActionsCharges;
+            e.ProficiencyBonus = dto.ProficienyBonus;
+            e.Notes = dto.Notes;
+            e.IsNpc = dto.IsNpc;
+            e.LairInitiative = dto.LairInitiative;
+            e.InitiativeBonus = dto.InitiativeBonus;
+            e.OwnerUserId = dto.OwnerUserId;
+        }
+
+        protected override Creature MapToEntity(CreateCreatureDto dto) => new()
+        {
+            Name = dto.Name,
+            Size = dto.Size,
+            TypeId = dto.TypeId,
+            Ac = dto.Ac,
+            Hp = dto.Hp,
+            HpDiceCount = dto.HpDiceCount,
+            HpDiceSize = dto.HpDiceSize,
+            HpDiceBonus = dto.HpDiceBonus,
+            LegendaryActionCharges = dto.LegendaryActionsCharges,
+            ProficiencyBonus = dto.ProficienyBonus,
+            Notes = dto.Notes,
+            IsNpc = dto.IsNpc,
+            LairInitiative = dto.LairInitiative,
+            InitiativeBonus = dto.InitiativeBonus,
+            OwnerUserId = dto.OwnerUserId
+        };
+
+        protected override Expression<Func<Creature, bool>> ById(int id) => u => u.Id == id;
+
+        protected override int GetKey(Creature entity) => entity.Id;
+    }
+}
